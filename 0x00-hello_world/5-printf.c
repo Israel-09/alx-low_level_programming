a123f52b98a67e59b1cf627abe2cdb4ea740ddf6
israel-09 <israelinene91@gmail.com>
2022-10-13 12:41:24 +0000

    tasks 5 and 6 upload

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints out a message using prinf function
+ * Return: 0 if program runs successfully. otherwise 1
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a ");
+	printf("piece of art,\n");
+	return (0);
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints the size of the differents data types on my pc
+ * Return: 0 if the program runs without error. otherwise 1
+ */
+int main(void)
+{
+
+	printf("Size of a char: %lu bytes(s)\n", sizeof(char));
+	printf("Size of an int: %lu bytes(s)\n", sizeof(int));
+	printf("Size of a long int: %lu bytes(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu bytes(s)\n", sizeof(long long int));
+	printf("Size of a float: %lu bytes(s)\n", sizeof(float));
+	return (0);
+}
