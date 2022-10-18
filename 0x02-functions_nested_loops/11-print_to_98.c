155468165aac2d257003ae39e8e2ce9476c2d904
israel-09 <israelinene91@gmail.com>
2022-10-18 23:55:16 +0000

    task 12 correction

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -33,15 +33,19 @@ void less_than_98(int n)
 		if (n < 0)
 		{
 			_putchar('-');
-			if (n <= -10)
+			if (n <= -100)
+			{
+			       	_putchar(-n / 100 + '0');
+				_putchar((-n / 10) % 10 + '0');
+				_putchar((-n % 10 + '0');
+			}
+			else if(n <= -10)
 			{
 				_putchar(-n / 10 + '0');
 				_putchar(-n % 10 + '0');
 			}
 			else
-			{
 				_putchar(-n + '0');
-			}
 		}
 		else if (n >= 10)
 		{
@@ -49,9 +53,7 @@ void less_than_98(int n)
 			_putchar(n % 10 + '0');
 		}
 		else
-		{
 			_putchar(n + '0');
-		}
 		if (n != 98)
 		{
 			_putchar(',');
