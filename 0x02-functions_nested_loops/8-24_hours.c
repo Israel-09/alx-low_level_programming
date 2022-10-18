1397b7563cf044e4ec9bb0ab45be99bb968eb75d
israel-09 <israelinene91@gmail.com>
2022-10-18 21:27:07 +0000

    task 9-upload

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints all the minutes in 24 hours.
+ */
+void jack_bauer(void)
+{
+	int min1;
+	int min2;
+	int hr1;
+	int hr2;
+
+	for(hr2 = '0'; hr2 <= '2'; hr2++)
+	{
+		for(hr1 = '0'; hr1 <= '9'; hr1++)
+		{
+			for(min2 = '0'; min2 <= '5'; min2++)
+			{
+				for(min1 = '0'; min1 <= '9'; min1++)
+				{
+					if(hr2 == '2' && hr1 > '3')
+					{
+						break;
+					}
+					else
+					{
+						_putchar(hr2);
+						_putchar(hr1);
+						_putchar(':');
+						_putchar(min2);
+						_putchar(min1);
+						_putchar('\n');
+					}
+				
+				}
+			}
+		}
+	}
+}
--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -9,6 +9,6 @@
 int main(void)
 {
 	
-	times_table();
+	jack_bauer();
 	return (0);
 }
