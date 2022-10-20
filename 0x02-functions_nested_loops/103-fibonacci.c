eb30ac276d214356ff21b207d14ea3a2ddbe443e
israel-09 <israelinene91@gmail.com>
2022-10-20 07:09:53 +0000

    fibonacci even sum

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - prints the first 50 fibonacci numbers
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	long int a, b, c, add;
+	
+	a = 1;
+	b = 2;
+	add = 2;
+
+	do{
+		c = a + b;
+		if (c % 2 == 0)
+		{
+			add += c;
+		}
+		a = b;
+		b = c;
+	} while (c < 4000000);
+
+	printf("%li\n", add);
+	return (0);
+}
+
