f0577ff232cac53cecb21d8c6edfdb626ea64d1d
israel-09 <israelinene91@gmail.com>
2022-10-21 23:37:10 +0000

    task 12 update

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,58 +1,32 @@
 #include "main.h"
 
 /**
- * greater_than -executes when n is greater than
- * @n: number to be printed
- */
-void greater_than(int n)
-{
-	if (n >= 1000)
-	{
-		_putchar(n / 1000 + '0');
-		_putchar((n / 100) % 10 + '0');
-		_putchar((n % 100) / 10 + '0');
-	}
-	else if (n >= 100)
-	{
-		_putchar(n / 100 + '0');
-		_putchar((n / 10) % 10 + '0');
-	}
-	else if (n >= 10)
-		_putchar(n / 10 + '0');
-	_putchar(n % 10 + '0');
-}
-
-/**
- * less_than - executes if n < 0
- * @n: number to be printed
- */
-void less_than(int n)
-{
-	_putchar('-');
-	if (n <= -1000)
-	{
-		_putchar(-n / 1000 + '0');
-			_putchar((-n / 100) % 10 + '0');
-		_putchar((-n % 100) / 10 + '0');
-	}
-	else if (n <= -100)
-	{
-		_putchar(-n / 100 + '0');
-		_putchar((-n / 10) % 10 + '0');
-	}
-	else if (n <= -10)
-	_putchar(-n / 10 + '0');
-	_putchar(-n % 10 + '0');
-
-}
-/**
  * print_number - prints number using putchar function
  * @n: is the number to be printed
  */
 void print_number(int n)
 {
-	if (n >= 0)
-		greater_than(n);
-	else
-		less_than(n);
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n >= 1000)
+	{
+		_putchar((n / 1000) + '0');
+		_putchar(((n / 100) % 10) + '0');
+		_putchar(((n % 100) / 10) + '0');
+	}
+	else if (n >= 100)
+	{
+		_putchar((n / 100) + '0');
+		_putchar((n / 10) % 10 + '0');
+	}
+	else if (n >= 10)
+	{
+		_putchar(n / 10 + '0');
+	}
+	_putchar(n % 10 + '0');
+	_putchar('\n');
+	
 }
