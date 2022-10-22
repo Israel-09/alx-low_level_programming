3060fe3d9e8c3742b95419d760283c9255c6f0db
israel-09 <israelinene91@gmail.com>
2022-10-22 21:50:56 +0000

    print_triangle function  upload

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -6,19 +6,18 @@
  */
 void print_triangle(int size)
 {
-	int i,j,k;
+	int i, j, k;
 
-	for (i = 1; i <= size; i++)
-	{
-		for (j = 1; j <= size; j++)
+	if (size > 0)
+		for (i = 1; i <= size; i++)
 		{
-			if (i < size)
-			{
-					_putchar(' ');
-			}
-			else
+			for (j = size - i; j > 0; j--)
+
+				_putchar(' ');
+			for (k = i; k > 0; k--)
 				_putchar('#');
+			_putchar('\n');
 		}
+	else
 		_putchar('\n');
-	}
 }
