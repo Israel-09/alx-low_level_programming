909edfcebaa6dedb176a9bbbfc4f8aaf2006a818
israel-09 <israelinene91@gmail.com>
2022-10-24 00:14:42 -0700

    reset to 98 upload

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98  - resets the value of the parameter to 98
+ * @*n: is the pointer to the variable.
+ * 
+ * Return: 98 always
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+reset_to_98 set the value of its parameter to 98
