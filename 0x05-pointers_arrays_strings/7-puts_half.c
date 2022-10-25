edf60f0bec32ced200bc4d4bbb38ddc9168b3ef9
israel-09 <israelinene91@gmail.com>
2022-10-24 18:54:07 -0700

    task 7 upload

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+  * puts_half - prints the second halp of a string
+  * @str: string to be manipulated
+  */
+void puts_half(char *str)
+{
+	int i, n;
+
+	for (i = 0; ; i++)
+	{
+		if (str[i] == '\0')
+			break;
+	}
+	i -= 1;
+	if ((i % 2 == 0))
+		n = i / 2;
+	else
+		n = (i - 1) / 2;
+
+	for (n = n; n <= i; n++)
+		_putchar(str[n]);
+	_putchar('\n');
+}
