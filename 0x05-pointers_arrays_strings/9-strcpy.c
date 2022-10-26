9061065318936c430baa5fc403ee2f8e3be95c44
israel-09 <israelinene91@gmail.com>
2022-10-26 22:44:50 +0000

    quick fix

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,17 +9,15 @@
   */
 char *_strcpy(char *dest, char *src)
 {
-	int i, len, j;
-	
-	for (len = 0;; len++)
+	int i, len;
+
+	for (len = 0; src[len] != '\0'; len++)
 	{
-		if (src[len] == '\0')
-			break;
 	}
-	
 	for (i = 0; i < len && src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
 	}
+	dest[i] = '\0';
 	return (dest);
 }
