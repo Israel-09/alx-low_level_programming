4bc619451e4bad2aa049b72bf7521626b37f0f83
israel-09 <israelinene91@gmail.com>
2022-10-28 00:34:39 +0000

    strncpy upload

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -20,9 +20,9 @@ char *_strncpy(char *dest, char *src, int n)
 	{
 		dest[i] = src[i];
 	}
-	if (n > i)
+	for ( ; i < n; i++)
+	{
 		dest[i] = '\0';
-	else
-		dest[j] = '\0';
+	}
 	return (dest);
 }
