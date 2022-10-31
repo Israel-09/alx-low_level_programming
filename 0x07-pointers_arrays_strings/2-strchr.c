9fd1cc45f30b3008aa2f0797e355a8d8fb78573d
israel-09 <israelinene91@gmail.com>
2022-10-31 17:12:13 +0000

    made null zero part of the string search

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,9 +9,12 @@
  */
 char *_strchr(char *s, char c)
 {
-	int i;
+	int i, j;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (j = 0; j != '\0'; j++)
+	{
+	}
+	for (i = 0; i <= j + 1; i++)
 	{
 		if (s[i] == c)
 		{
