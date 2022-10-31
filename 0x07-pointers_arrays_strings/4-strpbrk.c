8ada221489172f7d875724a40065565bc5cd27f2
israel-09 <israelinene91@gmail.com>
2022-10-31 19:09:51 +0000

    strpbrk upload and its prototype in main.h

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strpbrk - gets the  length of a prefix substring
+ * @s: string the be searched.
+ * @accept: the string to be checked
+ *
+ * Return: the the byte that matches s
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+	unsigned int change;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				return (s + i);
+			}
+
+		}
+	}
+	return (NULL);
+}
