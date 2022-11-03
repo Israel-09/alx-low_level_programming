4e2855a3b9893215ee21e7e41a368cddbd51a2a7
israel-09 <israelinene91@gmail.com>
2022-11-03 21:13:10 +0000

    string reverse push

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,37 +1,15 @@
 #include "main.h"
 
 /**
- * len - calculates thr length of string.
- * @s: tbe string to be calculated
- *
- * Return: the lenght
- */
-int len(char *s)
-{
-	if (*s == '\0')
-		return (0);
-	return (1 + len(s + 1));
-}
-
-/**
  * _print_rev_recursion - prints a string reversed using recursion
  * @s: string to be reversed
  */
 
 void _print_rev_recursion(char *s)
 {
-	int l;
-
-	l = len(s) - 1;
-	if (l == 0)
+	if (*s)
 	{
-		_putchar(s[0]);
-		_putchar('\n');
-		return;
-	}
-	else
-	{
-		_putchar(s[l]);
 		_print_rev_recursion(s + 1);
+		_putchar(*s );
 	}
 }
