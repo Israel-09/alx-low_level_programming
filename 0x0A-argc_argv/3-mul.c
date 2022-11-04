9e37c8d888a17730c57732ffe638b323124515a4
israel-09 <israelinene91@gmail.com>
2022-11-04 15:07:26 -0700

    3-mul.c upload

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - performs multiplication on the arguements of main.
+ * @argc: the number of arguements
+ * @argv: the array of arguements
+ *
+ * Return: 0 if main has arguements otherwise 1
+ */
+int main(int argc, char *argv[])
+{
+	int i, mul = 1;
+
+	if (argc - 1 == 0)
+	{
+		puts("Error");
+		return (1);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		mul *= atoi(argv[i]);
+	}
+	printf("%d\n",mul);
+	return 0;
+}
