4a4b3eb3204458d77c44cf5ea8de0861b0fc1f9b
israel-09 <israelinene91@gmail.com>
2022-11-04 14:51:52 -0700

    2-args.c upload

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints the arguemnts in receives
+ * @argc: the number of arguements
+ * @argv: array of arguements
+ *
+ * Return: 0 always.
+ */
+int main(int argc, char *argv[])
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
