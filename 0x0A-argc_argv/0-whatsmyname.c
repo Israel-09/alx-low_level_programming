83ac3f2678d8184d1e2946719a29584a47d11bca
israel-09 <israelinene91@gmail.com>
2022-11-04 19:48:29 +0000

    whatsmyname uplooad

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print whatever is passed as arguement
+ * @argc: the amount of arguements
+ * @argv: the pointer to array of arguments
+ *
+ * Return: always 0.
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
