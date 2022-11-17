a957a59d252a1d392bb9669717f1622f066b993e
israel-09 <israelinene91@gmail.com>
2022-11-17 13:48:56 -0800

    upload

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,10 @@
+#include "variadic_functions.h"
+
+/**
+ * print_all: prints all
+ * @format: lists all format in the string
+ */
+void print_all(const char * const format, ...)
+{
+	printf("mug");
+}
