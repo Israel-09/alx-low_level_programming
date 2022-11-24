d32732ddf2d09052b45c7f2569b8e61b7a412e61
israel-09 <israelinene91@gmail.com>
2022-11-24 08:37:31 -0800

    print list upload

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * len - finds the lenght of a string
+ * @str: - the string to be manipulated
+ *
+ * Return: the lenght of the string
+ */
+int len(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+	}
+	return (i);
+}
+/**
+ * print_list - prints all the element of a list
+ * @h: the pointer to the list
+ *
+ * Return: the number if nodes
+ */
+size_t print_list(list_t *h)
+{
+	size_t nodes = 0;
+
+	for (; h != NULL; h = h->next)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)");
+		else
+			printf("[%d] %s\n", len(h->str), h->str);
+		nodes += 1;
+	}
+	return (nodes);
+}
