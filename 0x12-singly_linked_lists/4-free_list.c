b8e6649605dd42678535088fcccec7c68e27d9fb
israel-09 <israelinene91@gmail.com>
2022-11-25 16:50:51 -0800

    free list free

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -5,3 +5,15 @@
  * free_list - frees the list
  * @head: the first node
  */
+
+void free_list(list_t *head)
+{
+	list_t *current;
+
+	while ((current = head) != NULL)
+	{
+		head = head->next;
+		free(current->str);
+		free(current);
+	}
+}
