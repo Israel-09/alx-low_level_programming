e7810131f4d09ae12508b2d5e32355d80c965578
israel-09 <israelinene91@gmail.com>
2022-12-05 16:56:56 -0800

    first file upload

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,28 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * read_textfile - reads the content of a trxt file int same dire tory
+ * @filename; the name of yhe file
+ * @letters: amount of letters to be read
+ *
+ * Return: the amount of lrtters printed
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	char buff[1024];
+	ssize_t p_char;
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	read(fd, buff, letters);
+	close(fd);
+	p_char = printf("%s\n", buff);
+	return (p_char);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,1 @@
+This contains programs on file description and inout/outpu handling.
