8612389bb6fdf04142197a53ffe4e110cf3c26cc
israel-09 <israelinene91@gmail.com>
2022-12-06 08:22:42 -0800

    betty compliat

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,9 +11,9 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd, w_check;
-	
+
 	if (!filename)
-		return(-1);
+		return (-1);
 	fd = open(filename, O_WRONLY, 0600);
 	if (fd != -1)
 	{
