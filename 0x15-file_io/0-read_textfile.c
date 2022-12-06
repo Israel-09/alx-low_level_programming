516b48b002d10728e9f22fc15dc1f1bea163fdf9
israel-09 <israelinene91@gmail.com>
2022-12-05 16:58:50 -0800

    added check for null pointer to.filename"

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -18,6 +18,8 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	char buff[1024];
 	ssize_t p_char;
 
+	if (!filename)
+		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
