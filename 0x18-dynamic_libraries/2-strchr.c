ae421a4a61662f87a6cce7dbdbc1f0d68255efcd
israel-09 <israelinene91@gmail.com>
2023-01-16 19:59:34 -0800

    libraru creation

---
+++ 0x18-dynamic_libraries/2-strchr.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strchr - locates the chracter c in string s
+ * @s: the string to be searched
+ * @c: the chracter to be located
+ *
+ * Return: the location of c.
+ */
+char *_strchr(char *s, char c)
+{
+	return(NULL);
+}
---
+++ 0x18-dynamic_libraries/README.md
@@ -0,0 +1,1 @@
+contains codes on dynamic libraries
