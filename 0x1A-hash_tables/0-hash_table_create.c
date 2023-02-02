58274a485e53150cba39d01b36599ff607f0e2fa
israel-09 <israelinene91@gmail.com>
2023-02-02 14:21:31 -0800

    file upload

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,13 @@
+#include "hash_tables_h"
+
+/**
+ * hash_table_create - this function creates a new hash tale
+ * @size: the size of the hash table
+ * Return: pointer to the newly created
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_tabme_t *new[size];
+
+	return (new);
+}
---
+++ 0x1A-hash_tables/README.md
@@ -0,0 +1,1 @@
+This repo contains code on Hash Table data structure
