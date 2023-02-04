cf3a91ce2993452aed03007f79b87586005e8916
israel-09 <israelinene91@gmail.com>
2023-02-03 18:18:35 -0800

    codeul upload-

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,7 +7,15 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *new[size];
+	hash_table_t *ht;
+	unsigned long int i;
 
-	return (*new);
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
+		return (NULL);
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_node_t *));
+	for (i = 0; i < size; i++)
+		ht->array[i] = NULL;
+	return (ht);
 }
