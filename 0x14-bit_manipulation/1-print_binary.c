255358fe0501551282d426b1388aa882ec571470
israel-09 <israelinene91@gmail.com>
2023-02-24 21:06:16 -0800

    file 1 upload

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- *  * print_binary - prints the binary of an integer
- *   * @n: the integer to be converted
- *    */
+ * print_binary - prints the binary of an integer
+ * @n: the integer to be converted
+ */
 void print_binary(unsigned long int n)
 {
 	unsigned long int current;
