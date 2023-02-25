5612268ca7cfb3b5420433e7aed6ccc0c65f24b9
israel-09 <israelinene91@gmail.com>
2023-02-24 21:40:35 -0800

    file upload

--- 0x14-bit_manipulation/main.c
+++ 0x14-bit_manipulation/main.c
@@ -1,23 +1,23 @@
-include <stdio.h>
+#include <stdio.h>
 #include "main.h"
 
 /**
  * main - check the code
   *
-  * Return: Always 0.
-  */
+   * Return: Always 0.
+    */
     int main(void)
     {
         unsigned long int n;
 
-	    n = 2048;
-//	        set_bit(NULL, 5);
+	    n = 1024;
+	        clear_bit(&n, 10);
 		    printf("%lu\n", n);
 		        n = 0;
-			    set_bit(&n, 2000);
+			    clear_bit(&n, 10);
 			        printf("%lu\n", n);
 				    n = 98;
-				        set_bit(&n, 0);
+				        clear_bit(&n, 1);
 					    printf("%lu\n", n);
 					        return (0);
 						}
