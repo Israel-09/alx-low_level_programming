c84a404831720e2fdcd221f0abc43d03a17722b1
israel-09 <israelinene91@gmail.com>
2023-02-25 06:22:44 -0800

    file upload

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * get_endianess - gets the endianess of a machine
+ * get_endianness - gets the endianess of a machine
  *
  * Return: 0 for big endian and 1 for small endian
  */
 int get_endianness(void)
 {
 	int i = 1;
-
 	char *c = (char *)&i;
+
 	if (*c)
 		return (1);
 	return (0);
