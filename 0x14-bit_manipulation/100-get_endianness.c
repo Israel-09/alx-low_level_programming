a7ac07ee9064b023127216f8cb4b3dfc68e6e9a1
israel-09 <israelinene91@gmail.com>
2023-02-25 06:21:17 -0800

    file upload

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * get_endianess - gets the endianess of a machine
+ *
+ * Return: 0 for big endian and 1 for small endian
+ */
+int get_endianness(void)
+{
+	int i = 1;
+
+	char *c = (char *)&i;
+	if (*c)
+		return (1);
+	return (0);
+}
--- 0x14-bit_manipulation/main.c
+++ 0x14-bit_manipulation/main.c
@@ -1,33 +1,18 @@
 #include <stdio.h>
 #include "main.h"
 
-/**
- * main - check the code
-<<<<<<< HEAD
-  *
-   * Return: Always 0.
-    */
 int main(void)
 {
-	printf("%d\n", 10^4);
-=======
- *
- * Return: Always 0.
- */
-int main(void)
-{
-        unsigned int n;
-	
-	n = flip_bits(1024, 1);
-	printf("%u\n", n);
-	n = flip_bits(402, 98);
-	printf("%u\n", n);
-	n = flip_bits(1024, 3);
-	printf("%u\n", n);
-	n = flip_bits(1024, 1025);
-	printf("%u\n", n);
+    int n;
 
-	print_binary(n);
-	return (0);
->>>>>>> refs/remotes/origin/master
-}
+        n = get_endianness();
+	    if (n != 0)
+	        {
+		        printf("Little Endian\n");
+			    }
+			        else
+				    {
+				            printf("Big Endian\n");
+					        }
+						    return (0);
+						    }
