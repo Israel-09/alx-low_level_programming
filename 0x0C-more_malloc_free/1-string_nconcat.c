b1283ae8849792aa973c9e0ae81be1f4144049aa
israel-09 <israelinene91@gmail.com>
2023-02-26 08:57:00 -0800

    code upload

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * _strlen - length of a string
+ * @s: the string
+ * Return: the string lenght
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		;
+	return (i);
+}
+
+/**
+ * string_nconcat - concatenates two string
+ * @s1: the first string
+ * @s2: the second string
+ * @n: thr nunber of characters of s2 to be copied.
+ * Return: pointer to the concatenated string
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	int i, j, len;
+	char *concat;
+
+	len = _strlen(s1);
+	i = _strlen(s2);
+	if (n >= (unsigned int)i)
+		len += i;
+	len += n;
+
+	concat = malloc(sizeof(char) * len);
+	if (concat == NULL)
+		return (NULL);
+
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		concat[i] = s1[i];
+	}
+	for (j = 0; i < len; i++, j++)
+	{
+		concat[i] = s2[j];
+	}
+	concat[i] = '\0';
+	return (concat);
+}
