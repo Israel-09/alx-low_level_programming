bfe95d6ea549c012e5e56189923434741d3d5b2c
israel-09 <israelinene91@gmail.com>
2023-02-26 09:02:30 -0800

    file load

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -26,13 +26,17 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	int i, j, len;
 	char *concat;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	len = _strlen(s1);
 	i = _strlen(s2);
 	if (n >= (unsigned int)i)
 		len += i;
 	len += n;
 
-	concat = malloc(sizeof(char) * len);
+	concat = malloc(sizeof(char) * len + 1);
 	if (concat == NULL)
 		return (NULL);
 
