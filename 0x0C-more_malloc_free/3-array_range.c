fe9bf5edd734a37acaf301d3eda244b3f6d53b29
israel-09 <israelinene91@gmail.com>
2023-02-26 09:57:45 -0800

    file upload

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * array_range - allocate memory for an array and store all digit min - max
+ * @min: the smallest element in the array
+ * @max: the biggest element in the array
+ *
+ * Return: pointer to the array
+ */
+int *array_range(int min, int max)
+{
+	int diff, i;
+	int *array;
+
+	if (min > max)
+		return (NULL);
+	diff = max - min;
+	if (diff == 0)
+		diff = 1;
+	array = malloc(sizeof(int) * diff);
+	if (array == NULL)
+		return (NULL);
+	for (i = 0; i < diff; i++)
+	{
+		arr[i] = min;
+
+		if (min == max)
+			break;
+		min++;
+	}
+	return (array);
+}
