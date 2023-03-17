0098a6fc3873771abd4cf628a9738b589dc24a11
israel-09 <israelinene91@gmail.com>
2023-03-17 04:21:13 +0000

    file 101 upload

---
+++ 0x17-doubly_linked_lists/main.py
@@ -0,0 +1,19 @@
+
+def find_multiple(num):
+    mul = 0;
+    for i in range(1000):
+        for  j in range(1000):
+            if i > 99 and j > 99:
+                mul = i * j
+                if (mul > 900000):
+                    s_mul = str(mul)
+                    lent = len(s_mul) - 1
+                    for k in range(len(s_mul)):
+                        if s_mul[k] != s_mul[lent]:
+                            break
+                        if (k == 2):
+                            print(s_mul)
+                            break
+                        lent -= 1
+
+find_multiple(997799)
