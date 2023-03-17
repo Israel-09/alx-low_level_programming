b6ea22d34c9c606b04559bf7438f9f3e507c5eb8
israel-09 <israelinene91@gmail.com>
2023-03-17 04:57:37 +0000

    code fix

--- 0x17-doubly_linked_lists/main.py
+++ 0x17-doubly_linked_lists/main.py
@@ -1,19 +1,21 @@
 
 def find_multiple(num):
     mul = 0;
+    max_list = []
     for i in range(1000):
         for  j in range(1000):
             if i > 99 and j > 99:
                 mul = i * j
-                if (mul > 900000):
+                if (mul > 100000):
                     s_mul = str(mul)
                     lent = len(s_mul) - 1
                     for k in range(len(s_mul)):
                         if s_mul[k] != s_mul[lent]:
                             break
                         if (k == 2):
-                            print(s_mul)
-                            return
+                            max_list.append(int(s_mul))
+                            break
                         lent -= 1
+    print(max(max_list))
 
-find_multiple(999 * 999)
+find_multiple(998001)
