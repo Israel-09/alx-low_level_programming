ddbde3133820f2bd3478a1ef7ebc8b5df9b39db8
israel-09 <israelinene91@gmail.com>
2023-04-08 23:01:31 -0700

    solution to taskn4

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,25 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - retrieves a value associat3d with a key
+ * @ht: the hash table
+ * @key: the key
+ *
+ * Return: the value associated to key on success otherwose NULL
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *current;
+	unsigned long int index;
+
+	index = key_index((const unsigned char *)key, ht->size);
+	current = ht->array[index];
+	while(current)
+	{
+		if (strcmp(key, current->key) == 0)
+			return (current->value);
+		current = current->next;
+	}
+
+	return (NULL);
+}
