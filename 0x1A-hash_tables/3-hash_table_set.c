8a1091c4abcbd92aae9b6f8ad57a1d8da2900f73
israel-09 <israelinene91@gmail.com>
2023-04-15 00:37:23 +0000

    solution to task 5 and 6

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,19 +12,22 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
 	hash_node_t *new_node;
-	
+
 	index = key_index((const unsigned char *)key, ht->size);
 	new_node = malloc(sizeof(hash_node_t));
 	if (!new_node || !key)
 		return (0);
 	new_node->key = strdup(key);
-	new_node->value = strdup(value);
-
-	if (ht->array[index] == NULL)
+	if (value)
 	{
-		ht->array[index] = new_node;
-		new_node->next = NULL;
+		new_node->value = malloc(sizeof(char) * strlen(value) + 1);
+		if (new_node->value == NULL)
+			return (0);
+		strcpy(new_node->value, value);
 	}
+	else
+		new_node->value = NULL;
+
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
 	return (1);
