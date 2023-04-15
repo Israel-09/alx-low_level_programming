70e7485151586c86a025988706375848263883fe
israel-09 <israelinene91@gmail.com>
2023-04-15 00:53:55 +0000

    task 5 solution

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -10,24 +10,25 @@ void hash_table_print(const hash_table_t *ht)
 	unsigned long int i = 0, j = 0;
 	hash_node_t *temp;
 
+	if (!ht)
+		return;
 	putchar('{');
-	if (ht)
-		for (i = 0; i < ht->size; i++)
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i])
 		{
-			if (ht->array[i])
+			temp = ht->array[i];
+			while (temp)
 			{
-				temp = ht->array[i];
-				while (temp)
-				{
-					if (j == 0)
-						printf("\'%s\': ", temp->key);
-					else
-						printf(", \'%s\': ", temp->key);
-					printf("\'%s\'", temp->value);
-					temp = temp->next;
-					j  = 1;
-				}
+				if (j == 0)
+					printf("\'%s\': ", temp->key);
+				else
+					printf(", \'%s\': ", temp->key);
+				printf("\'%s\'", temp->value);
+				temp = temp->next;
+				j  = 1;
 			}
 		}
+	}
 	printf("}\n");
 }
