e9ae39b5cda70cc49d7d02520e4c7d7254b5d896
israel-09 <israelinene91@gmail.com>
2023-08-09 02:16:25 +0000

    redirect error to POSIX stderr

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,6 +1,19 @@
 #include "main.h"
 
 /**
+ * print_err - print error message on the POSIX stderr
+ * @message: error message
+ * @arg: argument to the message
+ */
+void print_err(int fd, char *message, char *arg)
+{
+	write(fd, message, strlen(message));
+	write(fd, arg, strlen(arg));
+	write(fd, "\n", 1);
+}
+
+
+/**
  * copy_ft - the content of a file to another
  * @fd: array of the two file's fd
  * @av: array of array of the two filename
@@ -18,13 +31,13 @@ int copy_ft(int *fd, char **av)
 		r_chk = read(fd[0], buff, 1024);
 		if (r_chk == -1)
 		{
-			fprintf(stderr, "Error: Can't read from file %s\n", av[1]);
+			print_err(STDERR_FILENO, "Error: Can't read from file ", av[1]);
 			return (98);
 		}
 		w_chk = write(fd[1], buff, r_chk);
 		if (w_chk == -1)
 		{
-			fprintf(stderr, "Error: Can't write to file %s\n", av[2]);
+			print_err(STDERR_FILENO, "Error: Can't write to file ", av[2]);
 			return (99);
 		}
 		free(buff);
@@ -54,20 +67,26 @@ int main(int ac, char **av)
 	fd[0] = open(av[1], O_RDONLY);
 	if (fd[0] == -1)
 	{
-		fprintf(stderr, "Error: Can't read from file %s\n", av[1]);
+		print_err(STDERR_FILENO, "Error: Can't read from file ", av[1]);
 		return (98);
 	}
 
 	fd[1] = open(av[2], O_WRONLY | O_CREAT | O_TRUNC | O_APPEND, 00664);
 	if (fd[1] == -1)
 	{
-		fprintf(stderr, "Error: Can't write to %s\n", av[2]);
+		print_err(STDERR_FILENO, "Error: Can't write to file ", av[2]);
 		return (99);
 	}
 	exit_status = copy_ft(fd, av);
 	if (close(fd[0]) == -1)
+	{
+		fprintf(stderr, "Error: Can't close fd %d\n", fd[0]);
 		return (100);
+	}
 	if (close(fd[1]) == -1)
+	{
+		fprintf(stderr, "Error: Can't close fd %d\n", fd[1]);
 		return (100);
+	}
 	return (exit_status);
 }
